2963f8742eb9327a541b950448c749cdf4f81009
ganu <gwbaik9717@icloud.com>
2025-02-16 14:13:45 +0900

    feat: createElement 함수에 props 를 전달하면, 리턴 값의 props 속성에 props 들이 존재한다.

--- tests/createElement.test.js
+++ tests/createElement.test.js
@@ -3,13 +3,30 @@ import { createElement } from "../src/createElement";
 
 describe("createElement Unit Test", () => {
   test("createElement 함수는 파라미터로 type, props, children(Optional)을 받고 반환값으로 type, props 라는 속성을 가진 객체를 반환한다.", () => {
-    const type = "div";
-    const props = null;
+    const element = createElement("div", null);
 
-    const element = createElement(type, props);
+    expect(element).toEqual({
+      type: "div",
+      props: {},
+    });
+  });
+
+  test("createElement 함수에 props 를 전달하면, 리턴 값의 props 속성에 props 들이 존재한다.", () => {
+    const type = "div";
+    const id = "container";
+    const className = "flex";
+
+    const element = createElement(type, {
+      id,
+      className,
+    });
+
     expect(element).toEqual({
       type,
-      props: {},
+      props: {
+        id,
+        className,
+      },
     });
   });
 
