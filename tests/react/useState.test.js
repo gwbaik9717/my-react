731a26f2453c64d5fa8ba7c716ee80ed2a0a107c
ganu <gwbaik9717@icloud.com>
2025-02-22 13:54:54 +0900

    test: state 의 초기 값은 useState 의 parameter 로 전달된 initialValue 이다.

--- tests/react/useState.test.js
+++ tests/react/useState.test.js
@@ -18,4 +18,19 @@ describe("useState Unit Test", () => {
     expect(result.length).toBe(2);
     expect(typeof result[1]).toBe("function");
   });
+
+  test("state 의 초기 값은 useState 의 parameter 로 전달된 initialValue 이다.", () => {
+    const initialValue = 1;
+
+    const Component = () => {
+      const [state, setState] = React.useState(initialValue);
+
+      return state;
+    };
+
+    // Render Component
+    const result = Component();
+
+    expect(result).toBe(initialValue);
+  });
 });
