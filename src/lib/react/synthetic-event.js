bde3174631e2d96e92e6174edbbf58623b85c912
ganu <gwbaik9717@icloud.com>
2025-03-23 19:41:51 +0900

    feat: React Event System 간단한 구현

---
+++ src/lib/react/synthetic-event.js
@@ -0,0 +1,76 @@
+class BaseSyntheticEvent {
+  constructor(nativeEvent) {
+    this.nativeEvent = nativeEvent;
+    this.type = nativeEvent.type;
+    this.currentTarget = nativeEvent.currentTarget;
+    this.target = nativeEvent.target;
+  }
+}
+
+const createSyntheticEvent = (nativeEvent) => {
+  return new BaseSyntheticEvent(nativeEvent);
+};
+
+const eventMap = {
+  click: "onClick",
+  change: "onChange",
+};
+
+const findEventHandler = (syntheticEvent, reactRoot) => {
+  if (!reactRoot.current) {
+    throw new Error("Root must be initalized first");
+  }
+
+  const eventType = syntheticEvent.type;
+  const eventTarget = syntheticEvent.target;
+
+  const eventProp = eventMap[eventType];
+
+  if (!eventProp) {
+    throw new Error("Not registered Event");
+  }
+
+  const find = (reactElement) => {
+    if (reactElement.domNode === eventTarget) {
+      return reactElement.props[eventProp];
+    }
+
+    if (reactElement.child) {
+      const eventHandler = find(reactElement.child);
+
+      if (eventHandler) {
+        return eventHandler;
+      }
+    }
+
+    if (reactElement.sibling) {
+      const eventHandler = find(reactElement.sibling);
+
+      if (eventHandler) {
+        return eventHandler;
+      }
+    }
+  };
+
+  return find(reactRoot.current);
+};
+
+const handleEvent = (nativeEvent, reactRoot) => {
+  const syntheticEvent = createSyntheticEvent(nativeEvent);
+
+  const eventHandler = findEventHandler(syntheticEvent, reactRoot);
+
+  if (eventHandler) {
+    eventHandler();
+  }
+};
+
+export const listenToNativeEvents = (rootDomNode, reactRoot) => {
+  const eventNames = ["click", "change"];
+
+  for (const eventName of eventNames) {
+    rootDomNode.addEventListener(eventName, (nativeEvent) => {
+      handleEvent(nativeEvent, reactRoot);
+    });
+  }
+};
